6c1828fc6225de103e69b98cb3590ac3b3db6632
buglight <dmzhukov@outlook.com>
2023-12-03 23:56:03 +0400

    day 1 part 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,11 +1,35 @@
 use std::{io::BufReader, io::BufRead, fs::File};
 use anyhow::Result;
+use nom::IResult;
+use nom::branch::alt;
+use nom::bytes::complete::tag;
+use nom::combinator::{iterator, map};
 
 // -- Helpers ------------------------------------------------------------------
 
+fn parse_token(str: &str) -> IResult<&str, char> {
+    alt((
+        map(tag("one"), |_| '1'),
+        map(tag("two"), |_| '2'),
+        map(tag("three"), |_| '3'),
+        map(tag("four"), |_| '4'),
+        map(tag("five"), |_| '5'),
+        map(tag("six"), |_| '6'),
+        map(tag("seven"), |_| '7'),
+        map(tag("eight"), |_| '8'),
+        map(tag("nine"), |_| '9'),
+        map(tag("zero"), |_| '0'),
+        nom::character::complete::anychar
+    ))(str)
+}
+
 fn parse_callibration_value(str: &str) -> i32 {
-    let digits = str.chars()
-        .filter(|c| c.is_digit(10))
+    let mut tokens_iterator = iterator(str, parse_token);
+    let tokens = tokens_iterator.collect::<Vec<char>>();
+    let _ = tokens_iterator.finish();
+
+    let digits = tokens.iter()
+        .filter(|t| t.is_ascii_digit())
         .map(|c| c.to_digit(10).unwrap() as i32)
         .collect::<Vec<i32>>();
     10 * digits.first().unwrap() + digits.last().unwrap()
@@ -22,6 +46,11 @@ mod tests {
         assert_eq!(parse_callibration_value("aa1a2b"), 12);
         assert_eq!(parse_callibration_value("1a2b1"), 11);
         assert_eq!(parse_callibration_value("aaaa1aaaa"), 11);
+    }
+
+    #[test]
+    fn test_parse_callibration_value_with_words() {
+        assert_eq!(parse_callibration_value("abcseven1nine5abb"), 75);
     }
 }
 
